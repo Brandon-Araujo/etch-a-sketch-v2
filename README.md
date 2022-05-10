39ec060718796ee71e601b089837fe0f4144b722
Brandon Araujo <brandonaraujo07@gmail.com>
2022-05-10 14:30:05 -0500

    Added README.md, index.html, script.js, style.css and .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-This is an Etch A Sketch project utilizing HTML CSS and JavaScript
+This is an Etch-A-Sketch project utilizing HTML, CSS and JavaScript.
+
+Project Outcomes:
+-deepened my knowledge of DOM manipulation
+-utilized CSS Flexbox and Grid for layout 
+-focused on developing organization to the code and adding more comments
+
 
--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 
     let slider = document.getElementById('myRange');
     let output = document.getElementById('demo');
-    const containerDiv = document.querySelector('.container-div');
-    const eraseButton = document.querySelector('.eraseButton');
+    const grid = document.querySelector('.grid-div');
+    const eraseButton = document.querySelector('.erase-button');
+    const blackButton = document.querySelector('.black-button');
+    const rainbowButton = document.querySelector('.rainbow-button');
+    const colorPicker = document.querySelector('.color-picker');
+    const animation = document.querySelector('.blink-animation');
+    const button = document.getElementsByClassName('button');
+    let buttonArray = Array.from(button);
     let cellsArray = [];
-    let sizePrompt = '256';
     let sliderValue = 16;
-    output.innerHTML = slider.value;
+    output.innerHTML = slider.value + ' X ' + slider.value;
 
 
-          
-
-    //creates the grid cells
+  
+    //creates default grid
     function createGrid() {
-        for (let i = 0; i <= 81 * 81; i++) {
+        for (let i = 0; i <= 16 * 16; i++) {
             const div = document.createElement('div');
-            containerDiv.appendChild(div);
+            grid.appendChild(div);
             div.classList.add('cell');
         }
         const cell = document.querySelectorAll('.cell');
@@ -25,42 +29,82 @@
     }
 
 
-    //creates event listener for erase button
+    //buttons
     eraseButton.addEventListener('click', () => {
+        eraseButton.classList.add('blink-animation');
         cellsArray.forEach(element  => {
             colorChangeFunction(element, 'white');
         })
     })
 
+    blackButton.addEventListener('click', () => {
+        blackButton.classList.add('blink-animation');
+        cellsArray.forEach(element => {
+            colorChangeFunction(element, 'black');
+        })
+    })
+
+
+    rainbowButton.addEventListener('click', () => {
+        rainbowButton.classList.add('blink-animation');
+        cellsArray.forEach(element => {
+            element.addEventListener('mouseenter', () => {
+                element.style.cssText = `background-color: ${rainbowFunction()};`;
+            })
+        })
+    })
+
+    colorPicker.addEventListener('input', () => {
+        cellsArray.forEach(element => {
+            colorChangeFunction(element, event.target.value);
+        })
+    })
+
+    //removes animation class
+    buttonArray.forEach(element => {
+        element.addEventListener('animationend', () => {
+            element.classList.remove('blink-animation');
+        })
+    })
 
     //sets the active color
     function colorChangeFunction(element, color) {
             element.addEventListener('mouseenter', () => {
                 element.style.cssText = `background-color: ${color};`;
             })
-                 
+         
     }
-
-            
+    
+    //generates random RGB values
+    function rainbowFunction() {
+        let num1 = Math.floor(Math.random() * 256);
+        let num2 = Math.floor(Math.random() * 256);
+        let num3 = Math.floor(Math.random() * 256);
+        let rgbNum = 'rgb(' + num1 + ', ' + num2 + ', ' + num3 + ')';
+        return rgbNum;
+    }
+    
     //clears the grid
     function clearGrid() {
-        while (containerDiv.firstChild) {
-                containerDiv.removeChild(containerDiv.firstChild);
+        while (grid.firstChild) {
+             grid.removeChild(grid.firstChild);
         }
     }
 
+    //slider functionality
     slider.oninput = function() {
-        output.innerHTML = this.value;
+        output.innerHTML = this.value + ' X ' + this.value;
         sliderValue = Number(this.value);
         clearGrid();
         rePopulateGrid();
     }
 
+    //defines new grid when slider is toggled 
     function rePopulateGrid() {
-        containerDiv.style.cssText = `grid-template-rows: repeat(${sliderValue}, 1fr); grid-template-columns: repeat(${sliderValue}, 1fr);`;
+        grid.style.cssText = `grid-template-rows: repeat(${sliderValue}, 1fr); grid-template-columns: repeat(${sliderValue}, 1fr);`;
         for (let i = 0; i <= sliderValue * sliderValue; i++) {
             const div = document.createElement('div');
-            containerDiv.appendChild(div);
+            grid.appendChild(div);
             div.classList.add('cell');
         }
         const cell = document.querySelectorAll('.cell');
@@ -68,6 +112,5 @@
         cellsArray = makeArray;
         cellsArray.forEach(element => colorChangeFunction(element, 'black'));
     }
-            
-
+    
     createGrid();
